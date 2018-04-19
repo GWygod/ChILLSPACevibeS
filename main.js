f89632f265a5255ed36d99b497561dc6ecc35a30
brianwbermeo <33329484+brianwbermeo@users.noreply.github.com>
2018-04-19 15:00:30 -0400

    Updated emptySpace into Cell subclass

--- main.js
+++ main.js
@@ -7,12 +7,60 @@ const IMAGES = {
   conquered_space: 'https://i.imgur.com/9JvxNJg.jpg'
 };
 
+class emptySpace extends Cell {
+
+  init() {
+    this.status = 0;
+    this.conquerable == false;
+  }
+
+  get info() {
+    return 'This is empty space. You must traverse it to find other planets.'
+  }
+
+  // get image() {
+  //   return 'space'
+  // }
+
+  get color() {
+    return [40,40,40];
+  }
+
+  update(neighbors) {
+    var conquerable = this.conquerable
+    var self = this;
+
+    neighbors.forEach(function(neighbor) {
+      if (neighbor.item instanceof myemptySpace) {
+        conquerable = true
+      } else if (neighbor.item instanceof mySystem) {
+        conquerable = true
+      }
+    })
+  }
+
+  onClick() {
+    if (this.conquerable = true) {
+      this.status += 1;
+      if (this.status >= 1) {
+        STATE.resources.energy = STATE.resources.energy - 5
+        var mySpace = new myemptySpace
+        place(mySpace, this.x, this.y)
+        showMessage('You have conquered free space. Your troops are free to move.')
+      }
+    } else {
+        showMessage('You must reach this tile before conquering it, GOD-QUEEN.')
+    }
+  }
+}
+
 // REQUIRED: configure the grid
 const GRID_ROWS = 50;
 const GRID_COLS = 50;
 const GRID_CELL_SIZE = 75;
 const GRID_EMPTY = [234, 234, 234];
 const GRID_TYPE = 'hex';
+const GRID_DEFAULT_CELL = emptySpace;
 
 // REQUIRED: define how our resources will be represented
 const RESOURCES = {
@@ -32,7 +80,7 @@ const STATE = {
     army: 150,
     supplies: 30,
     morale: 90,
-    energy: 500,
+    energy: 100,
     money: 0,
   }
 }
@@ -66,58 +114,11 @@ class myemptySpace extends Item {
   }
 }
 
-class emptySpace extends Item {
-
-  init() {
-    this.status = 0;
-    this.conquerable == false;
-  }
-
-  get info() {
-    return 'This is empty space. You must traverse it to find other planets.'
-  }
-
-  get image() {
-    return 'space'
-  }
-
-  update(neighbors) {
-    var conquerable = this.conquerable
-    var self = this;
-
-    neighbors.forEach(function(neighbor) {
-      if (neighbor.item instanceof myemptySpace) {
-        conquerable = true
-      } else if (neighbor.item instanceof mySystem) {
-        conquerable = true
-      }
-    })
-  }
-
-  onClick() {
-    if (this.conquerable = true) {
-      this.status += 1;
-      if (this.status >= 1) {
-        STATE.resources.energy = STATE.resources.energy - 5
-        var mySpace = new myemptySpace
-        place(mySpace, this.x, this.y)
-        showMessage('You have conquered free space. Your troops are free to move.')
-      }
-    } else {
-        showMessage('You must reach this tile before conquering it, GOD-QUEEN.')
-    }
-  }
-}
-
-
 // Initial setup of the game
 function init() {
   var systems = new mySystem();
-  place(systems, 25, 25);
+  place(systems, 24, 24);
   STATE.systems += 1;
-
-  var space = new emptySpace();
-  place(space, 25, 26);
 
   // Setup the Menu for buying stuff
   var menu = new Menu('Intergalactic Bureau of Defense', [
