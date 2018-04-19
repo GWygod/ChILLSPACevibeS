9d12b2f253d1a8eff5955807f2e59fb3fd75fd9f
brianwbermeo <33329484+brianwbermeo@users.noreply.github.com>
2018-04-19 14:07:31 -0400

    New Subclass Update
    Added myemptySpace subclass
    Added emptySpace subclass

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 // REQUIRED: define images we want to use
 const IMAGES = {
-  flag: 'https://i.imgur.com/M8u25cr.png'
+  flag: 'https://i.imgur.com/M8u25cr.png',
+  space: 'https://i.imgur.com/hucO1lV.jpg'
 };
 
 // REQUIRED: configure the grid
@@ -10,7 +11,6 @@ const GRID_ROWS = 50;
 const GRID_COLS = 50;
 const GRID_CELL_SIZE = 75;
 const GRID_EMPTY = [234, 234, 234];
-const GRID_TYPE = 'hex';
 
 // REQUIRED: define how our resources will be represented
 const RESOURCES = {
@@ -37,7 +37,6 @@ const STATE = {
 
 class mySystem extends Item {
 
-
   get info() {
     return 'This system is part of your Hegemony, GOD-QUEEN!'
   }
@@ -55,18 +54,72 @@ class mySystem extends Item {
   }
 }
 
+class myemptySpace extends Item {
+  get info() {
+    return 'This is empty space that is under your protection. Your troops may move through it.'
+  }
+
+  get image() {
+    return 'flag'
+  }
+}
+
+class emptySpace extends Item {
+
+  init() {
+    this.status = 0;
+    this.conquerable == false;
+  }
+
+  get info() {
+    return 'This is empty space. You must traverse it to find other planets.'
+  }
+
+  get image() {
+    return 'space'
+  }
+
+  update(neighbors) {
+    var conquerable = this.conquerable
+    var self = this;
+
+    neighbors.forEach(function(neighbor) {
+      if (neighbor.item instanceof myemptySpace) {
+        conquerable = true
+      } else if (neighbor.item instanceof mySystem) {
+        conquerable = true
+      }
+    })
+  }
+
+  onClick() {
+    if (this.conquerable = true) {
+      this.status += 1;
+      if (this.status >= 1) {
+        STATE.resources.energy = STATE.resources.energy - 5
+        var mySpace = new myemptySpace
+        place(mySpace, this.x, this.y)
+        showMessage('You have conquered free space. Your troops are free to move.')
+      }
+    } else {
+        showMessage('You must reach this tile before conquering it, GOD-QUEEN.')
+    }
+  }
+}
+
 
 // Initial setup of the game
 function init() {
-  // Create a starting wheat plot
   var systems = new mySystem();
-  place(systems, 25, 30);
+  place(systems, 25, 25);
   STATE.systems += 1;
+
+  var space = new emptySpace();
+  place(space, 25, 26);
 
   // Setup the Menu for buying stuff
   var menu = new Menu('Intergalactic Bureau of Defense', [
   ]);
-
 }
 
 // The game's main loop.
